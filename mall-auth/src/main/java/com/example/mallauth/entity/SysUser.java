2055b17ee8e2d73ebc6f55b14c9b691df1b65a18
chenx <123456@qq.com>
2026-05-12 17:55:01 +0800

    登录信息管理

--- mall-auth/src/main/java/com/example/mallauth/entity/SysUser.java
+++ mall-auth/src/main/java/com/example/mallauth/entity/SysUser.java
@@ -11,33 +11,22 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SysUser {
-
     private Long id;
-
     private String username;
-
     private String password;
-
     private String nickname;
-
     private String phone;
-
     private String email;
-
     /**
      * 用户类型：1管理员，2商家，3用户
      */
     private Integer userType;
-
     /**
      * 状态：0禁用，1启用
      */
     private Integer status;
-
     private LocalDateTime createTime;
-
     private LocalDateTime updateTime;
-
     /**
      * 逻辑删除：0未删，1已删
      */
