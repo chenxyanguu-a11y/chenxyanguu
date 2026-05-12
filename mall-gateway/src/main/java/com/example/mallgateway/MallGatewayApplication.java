f51ff92e42ed542a398de80d174d27d85f2b97e8
chenx <123456@qq.com>
2026-05-12 17:53:47 +0800

    网关管理

--- mall-gateway/src/main/java/com/example/mallgateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/example/mallgateway/MallGatewayApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MallGatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MallGatewayApplication.class, args);
+
     }
 
 }
